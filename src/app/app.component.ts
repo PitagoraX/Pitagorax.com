c8c9efca290d1d4472ad97c07e2b6490964c8313
PitagoraX Ltd <pitagoraxltd@gmail.com>
2021-04-23 16:58:31 +0200

    Last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterEvent,
+} from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'WooWeb';
+  title = 'PitagoraX';
+  loading = true;
 
-  constructor(
-    public translate: TranslateService
-  ) {
-    
+  constructor(private router: Router, public translate: TranslateService) {
     setTimeout(() => {
       console.log('sleep');
     }, 5000);
 
     translate.addLangs(['en', 'es']);
     translate.setDefaultLang('en');
+
+    this.router.events.subscribe((e: RouterEvent) => {
+      this.navigationInterceptor(e);
+    });
   }
 
+  // Shows and hides the loading spinner during RouterEvent changes
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 }
-
-
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,37 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, SimpleChange } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   language = false;
   serSubmenu = false;
-  constructor(
-    public translate: TranslateService
-  ) {
-   
+  serSel = false;
+  proSubmenu = false;
+  proSel = false;
+  contactRoute = false;
+  constructor(public translate: TranslateService, private router: Router) {
+    this.router.events.subscribe((e: RouterEvent) => {
+      if (e instanceof NavigationEnd) {
+        if (e.url == '/cloud') {
+          this.serSel = true;
+        } else if (e.url == '/product') {
+          this.serSel = true;
+        } else {
+          this.serSel = false;
+        }
+        console.log(this.serSel);
+      }
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  changeLanguage(code: string)
-  {
+  changeLanguage(code: string) {
     this.translate.setDefaultLang(code);
     this.language = false;
   }
